963651099f3684073d1f98246e7a47da81c15f95
Vasileios Zois <vzois@usc.edu>
2016-12-02 18:20:10 -0800

    final project commit

--- Project/main.py
+++ Project/main.py
@@ -6,12 +6,17 @@ from cache import Cache
 from ram import RAM
 
 argc = len(sys.argv)
-if argc < 3:
-    print "Expecting trace file for simulation and example number(i.e. 1,2,3)!!!"
+if argc < 4:
+    print "Expecting trace file for simulation, example number(i.e. 1,2,3 memory levels) and (s)hort or (f)ull specs!!!"
     exit(1)
     
 trace_file = sys.argv[1]
 example = int(sys.argv[2])
+specs = sys.argv[3]
+p = False if specs == "s" else True
+
+    
+    
 compile()
 
 L1 = None
@@ -22,19 +27,22 @@ if example >=1:
     L1 = Cache("L1.cfg","L1")
     L1.model()
     add_mem(L1)
-    #L1.print_cfg()
+    if p:
+        L1.print_cfg()
 
 if example >=2:
     L2 = Cache("L2.cfg","L2")
     L2.model()
     add_mem(L2)
-    #L2.print_cfg()
+    if p:
+        L2.print_cfg()
 
 if example >=3:
     L3 = Cache("L3.cfg","L3")
     L3.model()
     add_mem(L3)
-    #L2.print_cfg()
+    if p:
+        L3.print_cfg()
 
 
 DDR = RAM("ram.cfg","DDR3")
